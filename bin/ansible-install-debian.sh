a0d2439e8e1a10480d4f65fe0cd0f468243d3abf
Noud de Brouwer <noud4@home.nl>
2020-06-17 23:21:22 +0200

    ansible-install-debian.sh

--- bin/ansible-install-debian.sh
+++ bin/ansible-install-debian.sh
@@ -2,6 +2,6 @@
 
 sudo apt update -y
 sudo apt install software-properties-common
-sudo apt-add-repository --yes --update "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main"
+sudo apt-add-repository --yes --update "deb http://ppa.launchpad.net/ansible/ansible/ubuntu eoan main"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 ./bin/ansible-install-ubuntu.sh
