bff1e8f4d54a91aa2c112d518b5ef229b1f72f72
Noud de Brouwer <noud4@home.nl>
2020-06-18 04:40:09 +0200

    centos typo

--- bin/ansible-install-centos.sh
+++ bin/ansible-install-centos.sh
@@ -2,4 +2,4 @@
 
 sudo yum-config-manager --add-repo -https://download.fedoraproject.org/pub/epel/$releasever/$basearch/
 sudo yum install python-jinja2 python-paramiko python2-crytography sshpass -y
-sudo rpm -ivh https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.9.9-1.el7.ans.noarch.rpm247
+sudo rpm -ivh https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.9.9-1.el7.ans.noarch.rpm
